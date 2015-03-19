29eefc70f4d636a6e25ec8b2f003b5218b1fbb64
Lajos Koszti <ajnasz@ajnasz.hu>
2015-03-19 15:06:13 +0100

    More structured test code

--- test/index.js
+++ test/index.js
@@ -6,6 +6,11 @@ var sinon = require('sinon');
 
 var pushbot = require('../src/pushbot');
 
+function rand() {
+	return Math.round(Math.random() * 100);
+}
+
+
 var robotProto = {
 	hear: function (regexp, cb) {
 		'use strict';
@@ -66,9 +71,9 @@ function createRobot() {
 	return robot;
 }
 
-function onMessage(robot, message, room, userName, userId) {
+function findCommand(robot, message) {
 	'use strict';
-	
+
 	var item = robot.__commands.reduce(function (result, item) {
 		if (result) {
 			return result;
@@ -79,28 +84,35 @@ function onMessage(robot, message, room, userName, userId) {
 		}
 	}, null);
 
+	return item;
+}
+
+function createMessage(robot, message, room, userName, userId) {
+
+	var item = findCommand(robot, message);
+
 	var msg = null;
 
 	if (item) {
 		msg = createMsg(item, message, room, userName, userId);
 	}
 
-	return {
-		item: item,
-		msg: msg
-	};
+	return msg;
 }
 
-
+function callCommand(command, msg) {
+	'use strict';
+	command.cb(msg);
+}
 describe('pushbot', function () {
 	'use strict';
 	var robot, room, bot, userName, userId;
 	beforeEach(function () {
-		room = 'Room-' + Math.round(Math.random() * 100);
+		room = 'Room-' + rand();
 		robot = createRobot();
 		bot = pushbot(robot);
-		userName = 'user-' + Math.round(Math.random() * 100);
-		userId = Math.round(Math.random() * 100);
+		userName = 'user-' + rand();
+		userId = rand();
 	});
 	afterEach(function () {
 		robot = null;
@@ -112,27 +124,27 @@ describe('pushbot', function () {
 		it('should add create a storage for room sessions', function () {
 			expect(robot.brain.data.pushbot[room]).to.be.an('undefined');
 
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 
 			expect(robot.brain.data.pushbot).to.be.an('object');
 			expect(robot.brain.data.pushbot[room]).to.be.an('array');
 		});
 
 		it('should add session to robot brain', function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 
 			expect(robot.brain.data.pushbot[room]).to.have.length(1);
 		});
 
 		it('should set properties to session', function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 
 			var session = robot.brain.data.pushbot[room][0];
 
-			expect(session).to.have.property('leader', res.msg.message.user.name);
+			expect(session).to.have.property('leader', msg.message.user.name);
 			expect(session).to.have.property('state', '');
 			expect(session).to.have.property('holded', false);
 			expect(session).to.have.property('holdMessage', '');
@@ -142,25 +154,25 @@ describe('pushbot', function () {
 		});
 
 		it('should add sender to users list', function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 
 			var session = robot.brain.data.pushbot[room][0];
 
 			expect(session.users).to.be.an('array');
 			expect(session.users).to.have.length(1);
 			expect(session.users[0]).to.be.an('object');
-			expect(session.users[0].name).to.deep.equal(res.msg.message.user.name);
+			expect(session.users[0].name).to.deep.equal(msg.message.user.name);
 
 			// TODO We should be able to set this from configuration
 			expect(session.users[0].state).to.deep.equal('waiting');
 		});
 
 		it('should create as many sessions, as many times called', function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
-			res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
+			msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 			var roomSessions = robot.brain.data.pushbot[room];
 
 			expect(roomSessions).to.have.length(2);
@@ -168,24 +180,24 @@ describe('pushbot', function () {
 	});
 
 	describe('.join with', function () {
-		var newUserName = 'user2-' + Math.round(Math.random() * 100),
-			newUserId = Math.round(Math.random());
+		var newUserName = 'user2-' + rand(),
+			newUserId = rand();
 		beforeEach(function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 		});
 		afterEach(function () {
 		});
 		it('should add a new session', function () {
-			var res = onMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
+			callCommand(findCommand(robot, '.join before ' + userName), msg);
 			var roomSessions = robot.brain.data.pushbot[room];
 
 			expect(roomSessions).to.have.length(2);
 		});
 		it('should before the other session, which has mentioned username', function () {
-			var res = onMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
+			callCommand(findCommand(robot, '.join before ' + userName), msg);
 			var roomSessions = robot.brain.data.pushbot[room];
 
 			expect(roomSessions[0]).to.have.property('leader', newUserName);
@@ -193,11 +205,11 @@ describe('pushbot', function () {
 		});
 
 		it('should have the same properties as the session with .join', function () {
-			var res = onMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join before ' + userName, room, newUserName, newUserId);
+			callCommand(findCommand(robot, '.join before ' + userName), msg);
 			var session = robot.brain.data.pushbot[room][0];
 
-			expect(session).to.have.property('leader', res.msg.message.user.name);
+			expect(session).to.have.property('leader', msg.message.user.name);
 			expect(session).to.have.property('state', '');
 			expect(session).to.have.property('holded', false);
 			expect(session).to.have.property('holdMessage', '');
@@ -208,7 +220,7 @@ describe('pushbot', function () {
 			expect(session.users).to.be.an('array');
 			expect(session.users).to.have.length(1);
 			expect(session.users[0]).to.be.an('object');
-			expect(session.users[0].name).to.deep.equal(res.msg.message.user.name);
+			expect(session.users[0].name).to.deep.equal(msg.message.user.name);
 
 			// TODO We should be able to set this from configuration
 			expect(session.users[0].state).to.deep.equal('waiting');
@@ -217,14 +229,14 @@ describe('pushbot', function () {
 
 	describe('.hold', function () {
 		beforeEach(function () {
-			var res = onMessage(robot, '.join', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
 		});
 		afterEach(function () {
 		});
 		it('should set session holded property to true', function () {
-			var res = onMessage(robot, '.hold hold message', room, userName, userId);
-			res.item.cb(res.msg);
+			var msg = createMessage(robot, '.hold hold message', room, userName, userId);
+			callCommand(findCommand(robot, '.hold hold message'), msg);
 
 			var session = robot.brain.data.pushbot[room][0];
 
@@ -232,19 +244,48 @@ describe('pushbot', function () {
 		});
 
 		it('should set session holdMessage property to given message', function () {
-			var msg = 'hold message - ' + Math.random(Math.random() * 100);
-			var res = onMessage(robot, '.hold ' + msg, room, userName, userId);
-			res.item.cb(res.msg);
+			var holdMessage = 'hold message - ' + rand();
+			var msg = createMessage(robot, '.hold ' + holdMessage, room, userName, userId);
+			callCommand(findCommand(robot, '.hold ' + holdMessage), msg);
 
 			var session = robot.brain.data.pushbot[room][0];
 
-			expect(session).to.have.property('holdMessage', msg);
+			expect(session).to.have.property('holdMessage', holdMessage);
 		});
 
 	});
 
-	describe.skip('.unhold', function () {});
-	describe.skip('.uhoh', function () {});
+	describe('.unhold', function () {
+		beforeEach(function () {
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
+		});
+		afterEach(function () {
+		});
+		it('should set holded channel to unhold', function () {
+			var holdMessage = 'hold message - ' + rand();
+			var msg = createMessage(robot, '.hold ' + holdMessage, room, userName, userId);
+			callCommand(findCommand(robot, '.hold ' + holdMessage), msg);
+
+			var session = robot.brain.data.pushbot[room][0];
+
+			expect(session).to.have.property('holdMessage', holdMessage);
+
+			msg = createMessage(robot, '.unhold', room, userName, userId);
+			callCommand(findCommand(robot, '.unhold'), msg);
+
+			expect(session).to.have.property('holded', false);
+			expect(session).to.have.property('holdMessage', '');
+		});
+	});
+	describe.skip('.uhoh', function () {
+		beforeEach(function () {
+			var msg = createMessage(robot, '.join', room, userName, userId);
+			callCommand(findCommand(robot, '.join'), msg);
+		});
+		afterEach(function () {
+		});
+	});
 	describe.skip('.good', function () {});
 	describe.skip('.nevermind', function () {});
 	describe.skip('.message', function () {});
@@ -254,16 +295,18 @@ describe('pushbot', function () {
 
 	describe('.clearplease', function () {
 		it('should remove all sessions from the room', function () {
-			var res = onMessage(robot, '.clearplease', room, userName, userId);
+			var msg = createMessage(robot, '.clearplease', room, userName, userId);
 
-			sinon.spy(res.msg, 'topic');
+			sinon.spy(msg, 'topic');
 
-			res.item.cb(res.msg);
+			var cmd = findCommand(robot, '.clearplease');
 
-			sinon.assert.called(res.msg.topic);
-			sinon.assert.calledWithExactly(res.msg.topic, '');
+			callCommand(cmd, msg);
 
-			res.msg.topic.restore();
+			sinon.assert.calledOnce(msg.topic);
+			sinon.assert.calledWithExactly(msg.topic, '');
+
+			msg.topic.restore();
 		});
 	});
 });
