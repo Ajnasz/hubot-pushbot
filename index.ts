073688afbf02ed8a0249ad31581ded70cc0e01c0
Lajos Koszti <ajnasz@ajnasz.hu>
2015-07-10 14:43:41 +0200

    Typescript the index.js

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 /*jshint node: true*/
 
+/// <reference path="typings/node.d.ts" />
+
 var fs = require('fs');
 
 var path = require('path');
 
-module.exports = function(robot, scripts) {
+module.exports = function (robot, scripts) {
 	'use strict';
-	var scriptsPath;
-	scriptsPath = path.resolve(__dirname, 'src');
-	return fs.exists(scriptsPath, function(exists) {
-		var i, len, ref, results, script;
+	let scriptsPath = path.resolve(__dirname, 'src');
 
+	return fs.exists(scriptsPath, function (exists) {
 		if (exists) {
 
-			ref = fs.readdirSync(scriptsPath);
-			results = [];
+			let ref = fs.readdirSync(scriptsPath);
+			let results = [];
 
-			for (i = 0, len = ref.length; i < len; i++) {
-				script = ref[i];
+			for (let i = 0, len = ref.length; i < len; i++) {
+				let script = ref[i];
 				if (scripts && scripts.indexOf('*') < 0) {
 					if (scripts.indexOf(script) > -1) {
 						results.push(robot.loadFile(scriptsPath, script));
