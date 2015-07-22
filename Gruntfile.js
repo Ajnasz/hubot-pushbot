5c389a9d24579e269411de820d4284187f182be2
Lajos Koszti <ajnasz@ajnasz.hu>
2015-07-23 00:04:04 +0200

    Build with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,32 @@
 /*jshint node: true*/
 module.exports = function (grunt) {
 	'use strict';
+	grunt.loadNpmTasks('grunt-typescript');
 	grunt.loadNpmTasks('grunt-release');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.initConfig({
+		typescript: {
+			base: {
+				src: 'src/pushbot.ts',
+				dest: 'src/pushbot-out.js',
+				target: 'ES5',
+				module: 'commonjs'
+			}
+		},
+		concat: {
+			base: {
+				src: ['COMMANDS.txt', 'src/pushbot-out.js'],
+				dest: 'src/pushbot.js'
+			}
+		},
+		clean: {
+			base: {
+				src: 'src/pushbot-out.js'
+			}
+		}
+	});
+
+	grunt.registerTask('compile', ['typescript', 'concat', 'clean']);
 };
